5fe52a4fd573b4265cd536ac61c408e31456b36b
Victor Santiago <vsantiago113sec@gmail.com>
2020-05-15 10:34:04 -0500

    Update

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(name='API-Client-Plugin-Boilerplate',
+      version='0.0.1',
+      description='A flexible and simple API Client Boilerplate to use it to build other API Clients that use RESTful.',
+      author='Victor M Santiago',
+      author_email='vsantiago113sec@gmail.com',
+      url='https://github.com/vsantiago113',
+      download_url='https://github.com/vsantiago113/API-Client-Plugin-Boilerplate',
+      license='MIT',
+      packages=['API-Client-Plugin-Boilerplate'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Topic :: Utilities',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+      ],
+      install_requires=[
+          'requests',
+          'urllib3',
+      ],
+      python_requires='>=3.4.0',
+      )
