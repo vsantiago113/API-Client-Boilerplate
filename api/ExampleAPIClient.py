fae5db27a6f4b82578200989a6ae1b7586a04c14
Victor Santiago <vsantiago113sec@gmail.com>
2020-05-15 10:51:52 -0500

    Update

--- api/ExampleAPIClient.py
+++ api/ExampleAPIClient.py
@@ -1,12 +1,18 @@
 from api import APIPlugin
+import requests
+from requests.auth import HTTPBasicAuth
 
 
 class Client(APIPlugin):
-    headers = None
+    headers = {'Content-Type': 'application/json'}
     base_url = None
 
     def connect(self, url: str = '', username: str = '', password: str = ''):
-        pass
+        self.base_url = f'{url.strip("/")}/api/{self.api_version}'
+        response = requests.post(f'{self.base_url}', auth=HTTPBasicAuth(username, password),
+                                 headers=self.headers, verify=self.verify)
+
+        return response
 
     def disconnect(self):
         pass
