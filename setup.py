c5b13c86ad71c98fa398fb4292a9eadb98e22cc2
Victor.Santiago_Cloud5 <Victor.Santiago@cloud5.com>
2021-08-20 16:01:58 -0500

    Added automation for the setup.py file. It get the install_requires from the requirements.txt file and finds the name of the repo, the latest tag and other information to make sure we don't miss important data in our setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,55 @@
 from setuptools import setup
+import subprocess
+import os
+
+# Find Latest Git Tag Available
+latest_git_tag_available = subprocess.check_output(['git', 'describe', '--tags']).decode('utf-8').strip()
+assert '.' in latest_git_tag_available
+
+get_origin_url = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url']).decode('utf-8').strip()
+assert 'http' in get_origin_url
+get_repo_name = get_origin_url.split('/')[-1].rstrip('.git')
+
+assert os.path.isfile('requirements.txt')
+with open('requirements.txt', 'r') as f:
+    requirements = []
+    for i in f.readlines():
+        if i:
+            requirements.append(i.rstrip())
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='API-Client-Boilerplate',
-    packages=['API-Client-Boilerplate'],
-    version='0.0.1',
+    name=get_repo_name,
+    packages=[get_repo_name],
+    version=latest_git_tag_available,
     license='MIT',
-    description='An API Client Boilerplate to build API Client that use RESTful in a standard way.',
+    description='A boilerplate to build API clients or wrappers.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Victor M Santiago',
     author_email='vsantiago113sec@gmail.com',
-    url='https://github.com/vsantiago113/API-Client-Boilerplate',
-    download_url='',
+    url=get_origin_url.rstrip('.git'),
+    download_url='{}/archive/{}.tar.gz'.format(get_origin_url.rstrip('.git'), latest_git_tag_available),
     keywords=['RESTful', 'Client', 'API', 'Boilerplate'],
-    python_requires='>=3.4.0',
-    install_requires=[
-        'requests',
-        'urllib3',
-    ],
-    classifiers=[
+    python_requires='>=3.6.0',
+    install_requires=requirements,
+    classifiers=[  # https://pypi.org/classifiers/
         'Development Status :: 5 - Production/Stable',
-        'Topic :: Utilities',
+        'Topic :: Internet :: WWW/HTTP',
         'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
